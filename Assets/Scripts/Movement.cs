1eb8f590243fb32a9def06bde9ae6cd281e9356e
HDonovan96 <harry.bd11@gmail.com>
2019-11-07 14:43:56 +0000

    Organised scripts by adding 'Raycaster' folder. Ray casting now only occurs is an object is within the boids view range

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,19 +10,45 @@ public class Movement : AbstractVisionCone
     public float visionRange;
     public float angle;
     public float fov;
+    private GameObject visionSphere;
+    private Vector3 safePath;
 
     void Start()
     {
+        // Create a vision sphere around each boid.
+        visionSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        visionSphere.transform.position = transform.position;
+        // Sets the vision spheres parent to the boid so that it moves with the boid
+        visionSphere.transform.parent = transform;
+        // Stop sphere being rendered and adds a trigger
+        visionSphere.GetComponent<MeshRenderer>().enabled = false;
+        visionSphere.GetComponent<SphereCollider>().isTrigger = true;
 
+        // Scales vision sphere to be the same as the vision range
+        visionSphere.transform.localScale = new Vector3(2 * visionRange, 2 * visionRange, 2 * visionRange);
 
+        //sets default value of safePath
+        safePath = transform.forward;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        other = visionSphere.GetComponent<Collider>();
+        safePath = GetPathAwayFromObjects(fov, angle, visionRange);
+
+        Debug.DrawRay(transform.position, safePath * visionRange, Color.yellow);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other = visionSphere.GetComponent<Collider>();
+        safePath = GetPathAwayFromObjects(fov, angle, visionRange);
+
+        Debug.DrawRay(transform.position, safePath * visionRange, Color.yellow);
     }
 
     void Update()
-    {
-        Vector3 safePath = GetPathAwayFromObjects(fov, angle, visionRange);
-
-        Debug.DrawRay(transform.position, safePath * visionRange, Color.yellow);
-
+    {       
         transform.forward = Vector3.RotateTowards(transform.forward, safePath, turnSpeed, 0.0f);
 
         transform.position += transform.forward * movementSpeed * Time.deltaTime;
